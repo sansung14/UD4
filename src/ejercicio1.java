06f08e19bc00b01a8347a7b0619ae162284a446b
sansung14 <78426816+sansung14@users.noreply.github.com>
2021-02-02 18:06:40 +0100

    resto

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -9,6 +9,7 @@ public class ejercicio1 {
 		System.out.println("Resta = "+n1+"-"+n2+" = "+(n1-n2));
 		System.out.println("Multiplicacion = "+n1+"x"+n2+" = "+(n1*n2));
 		System.out.println("División = "+n1+"/"+n2+" = "+(n1/n2));
+		System.out.println("División = "+n1+"%"+n2+" = "+(n1%n2));
 	}
 	
 }
